2be2bc6f39e7c8c47f00268544ac0adc847beb9b
Rajat Sharma <36663311+sharmarajat687@users.noreply.github.com>
2022-11-10 10:30:54 +0530

    updated

--- src/Counter.js
+++ src/Counter.js
@@ -9,9 +9,9 @@ function Counter() {
         <>
             <div style={{ width: "100%", padding: "25px 0px" }}>
                 <h2 style={{ fontSize: "56px" }}>{value}</h2>
-                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "gray" }} onClick={() => { updatedValue(value + 1) }}>Increase Value</button>
-                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "gray" }} onClick={() => { updatedValue(value - 1) }}>Decrease Value</button><br />
-                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "gray" }} onClick={() => { updatedValue(0) }}>Reset</button>
+                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "green", border: 0, margin: "10px" }} onClick={() => { updatedValue(value + 1) }}>Increase Value</button>
+                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "red", border: 0 }} onClick={() => { updatedValue(value - 1) }}>Decrease Value</button><br />
+                <button style={{ padding: "25px", fontSize: "26px", backgroundColor: "black", color: "white", border: 0 }} onClick={() => { updatedValue(0) }}>Reset</button>
             </div>
         </>
     )
